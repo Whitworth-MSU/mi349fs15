a3449c55ba0a40b64ff7869157e87b579103f5a6
Whitworth-MSU <whitwor9@msu.edu>
2015-12-10 23:30:01 -0500

    Finishing Final Website

--- Final/final.js
+++ Final/final.js
@@ -1,18 +1,5 @@
-$(document).ready(function() {
-    $('.hovertext').hide();
-    $('.hoverfade').animate({
-        opacity:1
-
-    });
-
-    $('.hoverfade').hover(function() {
-        $(this).stop().animate({opacity:.5},200);
-        $('.hovertext').fadeIn();
-
-    }, function() {
-        $(this).stop().animate({opacity:1},500)
-        $('.hovertext').fadeOut();
-    });
-
-
+$(function(){
+  $("p").click(function(){
+      $(this).hide();
+  });
 });
