ff53f592825f4ee1525cfb37b4f6435c85de4136
Whitworth-MSU <whitwor9@msu.edu>
2015-12-11 13:16:16 -0500

    rearranged where the final project info is found

---
+++ Final/Final Project/final.js
@@ -0,0 +1,15 @@
+$(function(){
+  $("#gamediv").click(function(){
+      $("#game1").toggle(500);
+      $("#game2").toggle(500);
+  });
+
+  $("#userdiv").click(function(){
+      $("#user1").toggle(500);
+      $("#user2").toggle(500);
+  });
+  $("#accountdiv").click(function(){
+      $("#account1").toggle(500);
+      $("#account2").toggle(500);
+  });
+});
--- Final/final.js
+++
@@ -1,5 +0,0 @@
-$(function(){
-  $("p").click(function(){
-      $(this).hide();
-  });
-});
